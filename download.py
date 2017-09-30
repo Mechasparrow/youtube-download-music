b903f726d5886fb91e21daa72df6d2f924796c79
Michael Navazhylau <mikipux7@gmail.com>
2017-09-29 22:05:15 -0500

    more progress

---
+++ download.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print (msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print ('Done downloading')
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': './output/hello.mp4',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
